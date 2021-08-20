30c300a08a8f60f5cf16073dd4e2980d3635bb51
Nikolay-Twin <twin-ss@mail.ru>
2021-08-20 10:45:03 +0300

    Update index.php

--- index.php
+++ index.php
@@ -3,3 +3,6 @@
     require __DIR__ . '/vendor/autoload.php';
     $config = include __DIR__ .'/resources/config.php';
     (new \app\composition\Assembler($config))->appRun();
+?>
+
+<h3>Начат процесс обработки.</h3> 
